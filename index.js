aedf79d72ead00aed127b546097854bb622c6b65
ducvan419 <duc19it@gmail.com>
2023-11-07 11:58:05 +0700

    handle feature edit task

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const grayColor = "#7D8592";
 const blueColor = "#713FFF";
-
 const iconAdd = document.querySelector(".task-btn-add");
 const buttonAdd = document.querySelector(".modal-btn-add");
-const iconEdit = document.querySelector(".task-edit-icon");
 const buttonEdit = document.querySelector(".modal-btn-edit");
 const modalAdd = document.querySelector(".modal-add");
 const modalUpdate = document.querySelector('.modal-update');
@@ -13,12 +11,12 @@ const buttonClose = document.querySelector('.modal-close-icon');
 const iconDelete = document.querySelector('.task-delete-icon');
 const buttonDelete = document.querySelector('.modal-delete-btn');
 const buttonCancel = document.querySelector('.modal-cancel-btn');
-const addInput = document.querySelector(".modal-input");
+const addInput = modalAdd.querySelector(".modal-input");
 const taskList = document.querySelector(".task-list");
 const highPriorityBtn = document.querySelector(".modal-btn-high");
 const mediumPriorityBtn = document.querySelector(".modal-btn-medium");
 const lowPriorityBtn = document.querySelector(".modal-btn-low");
-
+const modalHeading = modalAdd.querySelector(".modal-heading");
 const arrayTaskList = localStorage.getItem("arrayTaskList");
 const listTask = arrayTaskList ? JSON.parse(arrayTaskList) : [];
 
@@ -86,23 +84,26 @@ const updateSelected = () => {
     }
 }
 
-iconAdd.addEventListener("click", function () {
-    modalAdd.style.display = "block";
-    overlay.style.display = "block";
-
-    updateSelected();
-
+const checkValidateInput = () => {
     if (addInput.value == '') {
         buttonAdd.disabled = true;
         buttonAdd.style.cursor = "default";
         buttonAdd.style.border = `1px solid ${grayColor}`;
         buttonAdd.style.background = grayColor;
+    } else {
+        buttonAdd.disabled = false;
+        buttonAdd.style.cursor = "pointer";
+        buttonAdd.style.border = `1px solid ${blueColor}`;
+        buttonAdd.style.background = blueColor;
     }
-})
+}
 
-iconEdit.addEventListener("click", function () {
-    modalUpdate.style.display = "block";
+iconAdd.addEventListener("click", function () {
+    modalAdd.style.display = "block";
     overlay.style.display = "block";
+
+    updateSelected();
+    checkValidateInput();
 })
 
 buttonEdit.addEventListener("click", function () {
@@ -114,59 +115,63 @@ iconDelete.addEventListener("click", function () {
     overlay.style.display = "block";
 })
 
-
 let taskId = +localStorage.getItem("taskId") || 1;
 buttonAdd.addEventListener("click", function () {
+    if (buttonAdd.innerText == "Add") {
+        const newTask = {
+            taskId: taskId,
+            taskName: addInput.value,
+            priority: selectedPriority,
+        }
 
-    const newTask = {
-        taskId: taskId,
-        taskName: addInput.value,
-        priority: selectedPriority,
+        listTask.push(newTask);
+
+        taskId++;
+
+
+        localStorage.setItem("arrayTaskList", JSON.stringify(listTask));
+        localStorage.setItem("taskId", taskId);
+
+        const li = document.createElement("li");
+        li.classList.add("task-item");
+        li.setAttribute('id', `task-${taskId - 1}`);
+
+        li.innerHTML =
+            `
+            <div class="task-content">
+                <p class="task-content-heading">Task</p>
+                <p class="task-content-desc">${addInput.value}</p>
+            </div>
+            <div class="task-priority">
+                <p class="task-priority-heading">Priority</p>
+                <p class="task-priority-desc task-priority-${selectedPriority}">${selectedPriority}</p>
+            </div>
+            <div class="task-status">
+                <button class="task-status-btn">To Do</button>
+            </div>
+            <div class="task-icon-group">
+                <img src="./assets/images/status-todo.svg" alt="status icon" class="task-status-icon">
+                <img src="./assets/images/edit-icon.svg" alt="edit icon" class="task-edit-icon">
+                <img src="./assets/images/delete-icon.svg" alt="delete icon" class="task-delete-icon">
+            </div>
+        `;
+
+        taskList.insertBefore(li, taskList.firstChild);
+        addInput.value = '';
+        selectedPriority = "low";
+
+        modalAdd.style.display = "none";
+        overlay.style.display = "none";
+
     }
 
-    listTask.push(newTask);
-
-    taskId++;
-
-    localStorage.setItem("arrayTaskList", JSON.stringify(listTask));
-    localStorage.setItem("taskId", taskId);
-
-    const li = document.createElement("li");
-    li.classList.add("task-item");
-    li.setAttribute('id', `task-${taskId}`);
-
-    li.innerHTML =
-        `
-        <div class="task-content">
-            <p class="task-content-heading">Task</p>
-            <p class="task-content-desc">${addInput.value}</p>
-        </div>
-        <div class="task-priority">
-            <p class="task-priority-heading">Priority</p>
-            <p class="task-priority-desc task-priority-${selectedPriority}">${selectedPriority}</p>
-        </div>
-        <div class="task-status">
-            <button class="task-status-btn">To Do</button>
-        </div>
-        <div class="task-icon-group">
-            <img src="./assets/images/status-todo.svg" alt="status icon" class="task-status-icon">
-            <img src="./assets/images/edit-icon.svg" alt="edit icon" class="task-edit-icon">
-            <img src="./assets/images/delete-icon.svg" alt="delete icon" class="task-delete-icon">
-        </div>
-    `;
-
-    taskList.insertBefore(li, taskList.firstChild);
-    addInput.value = '';
-
-    selectedPriority = "low";
-
-    modalAdd.style.display = "none";
-    overlay.style.display = "none";
 })
 
 buttonClose.addEventListener("click", function () {
     addInput.value = '';
     selectedPriority = "low"
+    modalHeading.innerText = "Add Task";
+    buttonAdd.innerText = "Add";
     modalAdd.style.display = "none";
     overlay.style.display = "none";
 })
@@ -199,6 +204,53 @@ addInput.onkeyup = () => {
     }
 }
 
+//edit task
+const inputUpdate = modalUpdate.querySelector(".modal-input");
+taskList.addEventListener("click", function (event) {
+    const target = event.target;
+
+    if (target.classList.contains("task-edit-icon")) {
+        const li = target.closest("li");
+        const taskId = li.getAttribute("id");
+
+        modalAdd.style.display = "block";
+        overlay.style.display = "block";
+
+        const taskItemEdit = listTask[taskId.split('-')[1] - 1];
+        const taskName = taskItemEdit.taskName;
+        const priority = taskItemEdit.priority;
+        addInput.value = taskName;
+        selectedPriority = priority;
+        updateSelected();
+
+        modalHeading.innerText = "Edit Task";
+        buttonAdd.innerText = "Edit";
+        checkValidateInput();
+
+        buttonAdd.onclick = () => {
+            if (buttonAdd.innerText == "Edit") {
+                const taskContent = li.querySelector(".task-content-desc");
+                const taskPriority = li.querySelector(".task-priority-desc");
+                taskContent.innerText = addInput.value;
+                taskPriority.innerText = selectedPriority;
+                taskPriority.classList.remove(`task-priority-${priority}`);
+                taskPriority.classList.add(`task-priority-${selectedPriority}`);
+
+                taskItemEdit.taskName = addInput.value;
+                taskItemEdit.priority = selectedPriority;
+                localStorage.setItem("arrayTaskList", JSON.stringify(listTask));
+                modalAdd.style.display = "none";
+                overlay.style.display = "none";
+
+                modalHeading.innerText = "Add Task";
+                buttonAdd.innerText = "Add";
+
+                addInput.value = '';
+                selectedPriority = 'low';
+            }
+        }
+    }
+});
 
 
 
